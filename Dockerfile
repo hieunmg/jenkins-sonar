4db66f1549465cc8342b32a86c879f9af9a392eb
Hieu Minh <hieu.nguyen@pionero.io>
2023-09-05 23:46:01 +0700

    init

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM golang:1.19-alpine3.16 AS builder
+
+WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o main main.go
+
+
+# Run stage
+FROM alpine:3.16
+WORKDIR /app
+COPY --from=builder /app/main .
+EXPOSE 8080
+# CMD run whenever app start
+CMD [ "/app/main" ] 
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	}))
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
